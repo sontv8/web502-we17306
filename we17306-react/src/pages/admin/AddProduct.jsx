cabb376ecb5c82cb41dce6d4e978d5dd00a21bc2
sontv8 <sontv8@fpt.edu.vn>
2023-03-22 08:41:50 +0700

    Buoi 7: xu ly nhieu input

--- we17306-react/src/pages/admin/AddProduct.jsx
+++ we17306-react/src/pages/admin/AddProduct.jsx
@@ -2,21 +2,21 @@ import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const AddProductPage = (props) => {
-    const navigate = useNavigate()
-    const [inputValue, setInputValue] = useState()
+    const navigate = useNavigate() //khởi tạo navigate dùng để chuyển trang
+    const [inputValue, setInputValue] = useState() //khởi tạo state
     const onHandleChange = (e) => {
         // console.log(e.target.value);
         // setInputValue({ name: e.target.value })
         // const name = e.target.name //lấy ra name của input
         // const value = e.target.value //lấy ra value của input
-        const { name, value } = e.target
-        setInputValue({ ...inputValue, [name]: value });
+        const { name, value } = e.target //lấy ra name và value của input (destructuring)
+        setInputValue({ ...inputValue, [name]: value }); //gán giá trị cho state
     }
     const onHandleSubmit = (e) => {
         e.preventDefault();
         // console.log(inputValue);
-        props.onAdd(inputValue)
-        navigate('/admin/products')
+        props.onAdd(inputValue) //gọi hàm onAdd từ props
+        navigate('/admin/products') //chuyển về trang /admin/products
     }
     return (
         <div>
