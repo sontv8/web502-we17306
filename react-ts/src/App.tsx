9907563b5af9876d11516f27531bcff98e19dc23
sontv8 <sontv8@fpt.edu.vn>
2023-04-05 07:46:55 +0700

    Buổi 15: ant design - update

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,38 +1,35 @@
-import { useEffect, useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { addProduct, deleteProduct, getAllProduct, updateProduct } from './api/product'
 import AddProductPage from './pages/admin/AddProduct'
 import ProductManagementPage from './pages/admin/ProductManagement'
 import UpdateProductPage from './pages/admin/UpdateProduct'
 import HomePage from './pages/HomePage'
-import ProductsPage from './pages/Product'
+import ProductPage from './pages/Product'
 import ProductDetailPage from './pages/ProductDetail'
 import { IProduct } from './types/product'
 
 function App() {
-  const [products, setProduct] = useState([])
+  const [products, setProducts] = useState<IProduct[]>([])
   useEffect(() => {
-    getAllProduct().then(({ data }) => setProduct(data))
+    getAllProduct().then(({ data }) => setProducts(data))
   }, [])
   const onHandleRemove = (id: number) => {
-    deleteProduct(id).then(() => setProduct(products.filter((item: IProduct) => item.id !== id)))
+    deleteProduct(id).then(() => setProducts(products.filter((item: IProduct) => item.id !== id)))
   }
-  const onHandleAdd = (product) => {
-    addProduct(product)
+  const onHandleAdd = (product: IProduct) => {
+    addProduct(product).then(() => getAllProduct().then(({ data }) => setProducts(data)))
   }
-  const onHandleUpdate = (product) => {
-    updateProduct(product)
+  const onHandleUpdate = (product: IProduct) => {
+    updateProduct(product).then(() => getAllProduct().then(({ data }) => setProducts(data)))
   }
   return (
     <div className="App">
       <Routes>
         <Route path='/'>
           <Route index element={<HomePage />} />
-          <Route path='products'  >
-            <Route index element={<ProductsPage products={products} onRemove={onHandleRemove} />} />
-            <Route path=':id' element={<ProductDetailPage products={products} />} />
-          </Route>
-
+          <Route path='products' element={<ProductPage products={products} onRemove={onHandleRemove} />} />
+          <Route path='products/:id' element={<ProductDetailPage products={products} />} />
         </Route>
         <Route path='/admin'>
           <Route path='products'>
--- react-ts/src/pages/admin/UpdateProduct.tsx
+++ react-ts/src/pages/admin/UpdateProduct.tsx
@@ -1,28 +1,143 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
+import { IProduct } from '../../types/product'
+import { Button, Form, Input } from 'antd';
+interface IProps {
+    products: IProduct[],
+    onUpdate: (product: IProduct) => void
+}
 
-const UpdateProductPage = (props) => {
-    const { register, handleSubmit, reset } = useForm()
-    // khởi tạo hàm registern handleSubmit, reset từ useForm
-    //register dùng để đăng ký các input
-    // handleSubmit dùng để xử lý khi submit form
-    // reset dùng để reset lại giá trị trong form
-    const { id } = useParams() // lấy id từ url
-    useEffect(() => {
-        const currentProduct = props.products.find((item) => item.id === Number(id)) // tìm product có id trùng với id trong url
-        reset(currentProduct) // reset lại giá trị trong form
+// const UpdateProductPage = (props) => {
+//     const { id } = useParams() // lấy id từ url
+//     const [product, setProduct] = useState({}) // khởi tạo state product
+//     useEffect(() => {
+//         const product = props.products.find(item => item.id == id) // tìm product có id trùng với id trên url
+//         setProduct(product) // set lại giá trị cho state product
+//     })
+//     const [inputValue, setInputValue] = useState({}) // khởi tạo state inputValue
+//     const onHandleChange = (e) => { // hàm xử lý khi input thay đổi
+//         const { name, value } = e.target // lấy name và value của input
+//         setInputValue({ ...inputValue, [name]: value }) // set lại giá trị cho inputValue
+//     }
+//     const onHandleSubmit = (e) => {// hàm xử lý khi submit form
+//         e.preventDefault()
+//         const updateData = { ...product, ...inputValue } // gộp 2 object lại với nhau (nếu inputValue không có giá trị sẽ giữ nguyên giá trị cũ)
+//         props.onUpdate(updateData);
+//     }
+//     return (
+//         <div>
+//             <form action="" onSubmit={onHandleSubmit}>
+//                 <input type="text" defaultValue={product?.name} onChange={onHandleChange} name='name' />
+//                 <input type="number" defaultValue={product?.price} onChange={onHandleChange} name='price' />
+//                 <button type="submit">Update</button>
+//             </form>
+//         </div>
+//     )
+// }
+
+
+// const UpdateProductPage = (props) => {
+//     const navigate = useNavigate()
+//     const { register, handleSubmit, reset } = useForm()
+//     const { id } = useParams() // lấy id từ url
+//     useEffect(() => {
+//         const currentProduct = props.products.find(item => item.id == id) // tìm product có id trùng với id trên url
+//         reset(currentProduct) // reset lại form với giá trị của product
+//     }, [props])
+//     const onHandleSubmit = (data) => {
+//         props.onUpdate(data);
+//         navigate('/admin/products')
+//     }
+//     return (
+//         <div>
+//             <form action="" onSubmit={handleSubmit(onHandleSubmit)}>
+//                 <input type="text" {...register('name')} />
+//                 <input type="number" {...register('price')} />
+//                 <button type="submit">Update</button>
+//             </form>
+//         </div>
+//     )
+// }
+
+
+
+const UpdateProductPage = (props: IProps) => {
+    const { id } = useParams()
+    const navigate = useNavigate()
+    // const { register, handleSubmit, reset } = useForm()
+    // useEffect(() => {
+    //     const currentProduct = props.products.find((product) => product.id === Number(id))
+    //     reset(currentProduct)
+    // }, [props])
+    // const onHandleSubmit = data => {
+    //     props.onUpdate(data);
+    // }
+
+
+
+    const [product, setProduct] = useState<IProduct>() // khởi tạo biến state product có kiểu dữ liệu là IProduct
+    useEffect(() => { // khi props thay đổi thì sẽ chạy useEffect này
+        const currentProduct = props.products.find((product: IProduct) => product.id == Number(id))
+        // tìm trong mảng props.products có phần tử nào có id trùng với id trên url không
+        setProduct(currentProduct) // nếu có thì set lại giá trị cho biến product
     }, [props])
-    const onHandleSubmit = data => { // hàm xử lý khi submit form
-        props.onUpdate(data);// gọi hàm onUpdate từ props truyền vào
+    useEffect(() => { // khi biến product thay đổi thì sẽ chạy useEffect này
+        setFields() // gọi hàm setFields để set lại giá trị cho các input
+    }, [product])
+    const [form] = Form.useForm();
+    // khởi tạo một instance của Form và gán vào biến form
+    // Instance của form là một đối tượng được tạo ra bởi Ant Design để thực hiện các chức năng của form trong React
+
+    const setFields = () => {// hàm này để set lại giá trị cho các input
+        form.setFieldsValue({ // gọi hàm setFieldsValue của instance form để set lại giá trị cho các input dựa vào giá trị của biến product
+            id: product?.id,
+            name: product?.name,
+            price: product?.price
+        })
     }
+
+    const onFinish = (values: any) => {
+        props.onUpdate(values);
+        navigate('/admin/products')
+    };
+
     return (
         <div>
-            <form action="" onSubmit={handleSubmit(onHandleSubmit)}>
-                <input type="text" {...register('name')} />
-                <input type="number" {...register('price')} />
-                <button type="submit">Update Product</button>
-            </form>
+            <Form form={form} style={{ maxWidth: 600 }} onFinish={onFinish} >
+                {/* đoạn này cần truyền cả id vào form khi submit để lấy được giá trị id truyền lên component App */}
+                <Form.Item
+                    label=""
+                    name="id"
+                    style={{ display: 'none' }} // ẩn input này đi
+                    rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                    <Input />
+                </Form.Item>
+
+
+                <Form.Item
+                    label="Product Name"
+                    name="name"
+                    rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="Product Price"
+                    name="price"
+                    rules={[{ required: true, message: 'Please input your password!' }]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                    <Button type="primary" htmlType="submit">
+                        Update Product
+                    </Button>
+                </Form.Item>
+            </Form>
         </div>
     )
 }
